95a5cc09e1947620216bbe3d8535f7d0d36502e2
eastorga <eastorga0604@gamil.com>
2025-04-29 09:22:21 -0400

    added changes for deleting tgas and individual option

--- __manifest__.py
+++ __manifest__.py
@@ -5,12 +5,13 @@
     'category': 'Custom',
     'author': 'Tu Nombre',
     'description': 'Módulo para integrar WooCommerce con Odoo a través de una API personalizada.',
-    'depends': ['base','sale','crm'],
+    'depends': ['base','sale','crm', 'contacts'],
     'data': [
         'security/ir.model.access.csv',
         'views/sale_order_views.xml',
         'views/financing_agency_menu.xml',
-        'views/account_move_views.xml'
+        'views/account_move_views.xml',
+        'views/crm_quick_create_patch.xml',
     ],
     'controllers': [
         'controllers/main.py',
@@ -19,6 +20,7 @@
     'assets': {
         'web.assets_backend': [
             '/leasymat/static/src/js/kanban_quick_create_patch.js',  # <-- ADD THIS
+            '/leasymat/static/src/css/custom_styles.css',
         ],
     },
     'installable': True,
