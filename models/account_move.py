62e44632556a5a333cc5dbaa9804050b046f05d3
eastorga <eastorga0604@gamil.com>
2025-07-03 19:11:03 -0400

    added changes for the invoices styling

--- models/account_move.py
+++ models/account_move.py
@@ -15,6 +15,30 @@ class AccountMove(models.Model):
     amount_vat_20 = fields.Monetary(string='IVA 20%', compute='_compute_iva_20')
     amount_total_incl_vat_20 = fields.Monetary(string='Total Incl. IVA', compute='_compute_iva_20')
 
+    custom_display_number = fields.Char(string="Custom Invoice Number", readonly=True)
+
+    def _compute_custom_display_number(self):
+        for record in self:
+            if not record.invoice_date:
+                record.custom_display_number = ''
+                continue
+
+            date_str = record.invoice_date.strftime('%Y%m%d')
+            domain = [
+                ('move_type', '=', record.move_type),
+                ('state', '=', 'posted'),
+                ('invoice_date', '=', record.invoice_date),
+            ]
+            count = self.search_count(domain) + 1
+            record.custom_display_number = f'{date_str}-{count:02d}'
+
+    def action_post(self):
+        res = super().action_post()
+        for move in self:
+            if not move.custom_display_number:
+                move._compute_custom_display_number()
+        return res
+
     @api.depends('amount_total')
     def _compute_iva_20(self):
         for move in self:
